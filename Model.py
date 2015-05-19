d833321bd30220ae209f6be918d96fe0de1b444e
Gabriel H. Ortiz-Pena <gabo.ortiz@hotmail.com>
2015-05-19 11:57:11 -0400

    Version 1.0
    This is the first fully functional modeling code. Anything after this
    will be improvements on the physical viability, and might be
    implemented in separate forks/files to maintain clarity.

--- Model.py
+++ Model.py
@@ -1,7 +1,7 @@
 def main():
     import numpy as np
     import matplotlib
-    #matplotlib.use('PS')
+    matplotlib.use('PS')
     import matplotlib.pyplot as plt
     import Coordinates as coords
 
@@ -12,21 +12,21 @@ def main():
     P = 7200 # rotation period of the star, in seconds
     B_0 = 0.256 # magnetic field strength at the radius of the star, in Tesla
     pi = np.pi # pi
-    inc = pi/3 # inclination between the rotation axis and the line of sight
-    d = pi/3 # inclination angle between the rotation axis and the magnetic dipole axis    
+    inc = pi/2 # inclination between the rotation axis and the line of sight
+    d = pi/2 # inclination angle between the rotation axis and the magnetic dipole axis    
     # the local electron cyclotron frequency at the radius of the star, for v/c = 0.1; in Hz
     f_0 = e_0 * (B_0/(2*pi*m)) * (np.sqrt(1 - 0.1**2))
 
     pos = np.array([])
-    phase = np.array([(i%P)/float(P) for i in range(3*P)]) # phase, to plot the results
+    phase = np.array([i/float(P) for i in range(2*P)]) # phase, to plot the results
     I = np.array([])
     f = np.array([])
-    for t in range(3*P):
+    for t in range(2*P):
         phis = np.array([i*5 * 2*pi/P for i in range(10)]) 
         phis = phis + (t%P)*((2*pi)/P) # making 10 relevant lines, offset by 5 seconds
 
         # Each line will be at r = L, theta = i in the rotating frame
-        lines = np.array([coords.Spherical([L, pi/2, phis[i]]) for i in range(10)])
+        lines = np.array([coords.Spherical([L, inc, phis[i]]) for i in range(10)])
 
         for i in range(10):
             lines[i] = lines[i].rotate(d) # First the coordinates are rotated into the dipole frame
@@ -37,31 +37,36 @@ def main():
             # perp is the beaming angle in the shell electron distribution
             perp = pi/2 - np.arccos(2 * np.cos(lines[i].q['theta']) / np.sqrt(1 + 3 * np.cos(lines[i].q['theta'])**2))
             # and finally, determining the intensity of the beamed emission
-            mu = (lines[i].q['theta'] - perp)/(2*pi)
-            I = np.append(I, np.exp(-np.power(mu, 2.) / (2 * np.power(0.01, 2.)))/np.sqrt(2*pi*0.0001))
+            mu = abs((lines[i].q['theta'] - perp)/(2*pi))
+            if lines[i].q['theta'] < pi/2:
+                CP = 1
+            else:
+                CP = -1
+            I = np.append(I, CP * np.exp(-np.power(mu, 2.) / (2 * np.power(0.01, 2.)))/np.sqrt(2*pi*0.0001))
 
         pos = np.append(pos, lines)
 
-    pos.shape = (3*P, 10)
-    f.shape = (3*P, 10)
-    I.shape = (3*P, 10)
+    pos.shape = (2*P, 10)
+    f.shape = (2*P, 10)
+    I.shape = (2*P, 10)
 
     # This section is simply constructing the 2-dimensional array so it's simple to view using matplotlib
     f = f * 1000
     t = phase*P
-    flux = np.zeros((1001, 3*P))
-    for i in range(3*P):
+    flux = np.zeros((1001, 2*P))
+    for i in range(2*P):
         for j in range(10):
             flux[f[i, j], t[i]] = I[i, j]
 
             
-    dims = [0, 3, 0, 1]
+    dims = [0, 2, 0, 1]
     plt.imshow(flux, aspect = 'auto', origin = 'lower', extent = dims, cmap='gray')
     plt.ylabel(r"$\frac{f}{f_{B_0}}$")
     plt.xlabel(r"Phase")
     plt.title("Relative Intensity")
     plt.colorbar()
-    #plt.savefig('../../Desktop/DynSim/Spectrum')
+    plt.savefig('../../Desktop/DynSim/Spectrum')
+    #plt.plot(phase, I[:, 1])
     plt.show()
 
 if __name__ == '__main__':
